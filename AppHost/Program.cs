3ebfb3bb45abb469ee51e0bd0861f0fefff23ab0
nikolopk <kosnikolopoulos@hotmail.com>
2025-04-22 16:44:09 +0300

    Clear files

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -5,6 +5,6 @@ var redis = builder.AddRedis("cache");
 builder.AddProject<Projects.MySailorApi>("api")
     .WithReference(redis)
     .WaitFor(redis)
-    .WithReplicas(1);
+    .WithReplicas(5);
 
 builder.Build().Run();
